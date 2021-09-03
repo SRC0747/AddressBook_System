59c1405c8562f80b18719d60f5bbaedca3066399
SRC0747 <87673329+SRC0747@users.noreply.github.com>
2021-09-04 01:09:28 +0530

    Update AddressBook.java

--- src/test/java/com/addressbooksystem/AddressBook.java
+++ src/test/java/com/addressbooksystem/AddressBook.java
@@ -9,6 +9,7 @@ public class AddressBook {
         contactNewPerson();
         editContactDetail();
         clearConsole();
+        multiplePersonDetail();
     }
     public static void personDetail()
     {
@@ -73,5 +74,23 @@ public class AddressBook {
         {
             e.printStackTrace();
         }
+    }
+    public static void multiplePersonDetail()
+    {
+        List<String> langs = new ArrayList<>();
+
+        langs.add("Samppriti");
+        langs.add("Santragachi Station");
+        langs.add("kolkata");
+        langs.add("West Bengal");
+        langs.add("96748523145");
+        langs.add("sampritirc@hmail.com");
+
+        for (String lang : langs) {
+
+            System.out.printf("%s ", lang);
+        }
+
+        System.out.println();
     }
 }
