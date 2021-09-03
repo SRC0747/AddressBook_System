22d45272c606f1b3f843ee1dc5bb201b548852af
SRC0747 <RjSrc@0747>
2021-09-04 00:35:43 +0530

    added: Addding Contact details of New Person.

--- src/test/java/com/addressbooksystem/AddressBook.java
+++ src/test/java/com/addressbooksystem/AddressBook.java
@@ -6,6 +6,7 @@ public class AddressBook {
     public static void main(String[] args){
         System.out.println("Welcome to Address Book System.");
         personDetail();
+        contactNewPerson();
     }
     public static void personDetail()
     {
@@ -28,5 +29,27 @@ public class AddressBook {
         System.out.println("Phone number is:"+ph_no);
         String email_id = sc.nextLine();
         System.out.println("Email id is:"+email_id);
+    }
+    public static void contactNewPerson()
+    {
+        Scanner sc=new Scanner(System.in);
+        Console c=System.console();
+        System.out.println("Enter your name: ");
+        String n=c.readLine();
+        System.out.println("Welcome "+n);
+        System.out.println("Enter your address: ");
+        String address=c.readLine();
+        System.out.println("Your address is: "+address);
+        System.out.println("Enter your state: ");
+        String state=c.readLine();
+        System.out.println("Your State is: "+state);
+        System.out.println("Enter your city: ");
+        String city=c.readLine();
+        System.out.println("Your city is: "+city);
+        int ph_no=sc.nextInt();
+        System.out.println("Contact no: is:"+ph_no);
+        System.out.println("Enter your email:");
+        String email=c.readLine();
+        System.out.println("Your email is: "+email);
     }
 }
